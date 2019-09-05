2e6b0a1dab5d281ac76294cb8e037ecb1d7efa7a
-Vishnu Sharma <vbochiw1@in.ibm.com>
2019-09-05 15:52:38 +0530

    comppleted favourite list api

--- src/main/java/in/stackroute/hackathon/moviebackend/favourite/controller/FavouriteController.java
+++ src/main/java/in/stackroute/hackathon/moviebackend/favourite/controller/FavouriteController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -15,39 +16,29 @@ import java.util.TreeMap;
 public class FavouriteController {
 
     @Autowired
-    private FavouriteServiceInterface favouriteServiceInterface;
+    private FavouriteServiceInterface favouriteService;
 
     @PostMapping("favourites")
     ResponseEntity<Map> addFavourite(@RequestParam("username") String username, @RequestBody Favourite favourite) {
-        favouriteServiceInterface.addFavourite(username, favourite);
-        Map<String, Object> map = new TreeMap<>();
-        map.put("status", HttpStatus.CREATED);
-        map.put("data", favourite);
-        map.put("message", "Movie added to favourite.");
+        Map map = favouriteService.addFavourite(favourite);
         return new ResponseEntity(map, HttpStatus.CREATED);
     }
 
     @DeleteMapping("favourites/{favId}")
     ResponseEntity<Map> removeFavourite(@RequestParam("username") String username, @PathVariable("favId") Integer favId) {
-        Map<String, Object> map = new TreeMap<>();
-        map.put("status", HttpStatus.CREATED);
-        map.put("message", "Movie added to favourite.");
+        Map map = favouriteService.removeFavourite(favId);
         return new ResponseEntity(map, HttpStatus.CREATED);
     }
 
     @PutMapping("favourites")
     ResponseEntity<Map> editComment(@RequestParam("username") String username, @RequestBody Favourite favouriteDetail) {
-        Map<String, Object> map = new TreeMap<>();
-        map.put("status", HttpStatus.CREATED);
-        map.put("message", "Movie added to favourite.");
+        Map map = favouriteService.editComment(favouriteDetail);
         return new ResponseEntity(map, HttpStatus.CREATED);
     }
 
     @GetMapping("favourites")
     ResponseEntity<Map> getFavouriteByUsername(@RequestParam("username") String username) {
-        Map<String, Object> map = new TreeMap<>();
-        map.put("status", HttpStatus.CREATED);
-        map.put("message", "Movie added to favourite.");
+        Map map = favouriteService.getFavouriteByUsername(username);
         return new ResponseEntity(map, HttpStatus.CREATED);
     }
 
--- src/main/java/in/stackroute/hackathon/moviebackend/favourite/repo/FavouriteRepo.java
+++ src/main/java/in/stackroute/hackathon/moviebackend/favourite/repo/FavouriteRepo.java
@@ -3,5 +3,10 @@ package in.stackroute.hackathon.moviebackend.favourite.repo;
 import in.stackroute.hackathon.moviebackend.favourite.model.Favourite;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface FavouriteRepo extends MongoRepository<Favourite, Integer> {
+
+    List<Favourite> findAllByUsername(String username);
+
 }
