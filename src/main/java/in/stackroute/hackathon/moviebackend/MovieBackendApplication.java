c01bc23146aa4f30c95c84def881ed4ba62a0fac
Saswat <saswatbe@gmail.com>
2019-09-05 15:50:58 +0530

    Movie part added in backend

--- src/main/java/in/stackroute/hackathon/moviebackend/MovieBackendApplication.java
+++ src/main/java/in/stackroute/hackathon/moviebackend/MovieBackendApplication.java
@@ -1,13 +1,20 @@
 package in.stackroute.hackathon.moviebackend;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MovieBackendApplication {
+public class MovieBackendApplication implements CommandLineRunner{
 
     public static void main(String[] args) {
         SpringApplication.run(MovieBackendApplication.class, args);
     }
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		
+	}
 
 }
