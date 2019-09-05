b1ecddbd371d7175329aa21e09d7858ed3ac039a
-Vishnu Sharma <vbochiw1@in.ibm.com>
2019-09-05 19:58:59 +0530

    bug fixed.

--- src/main/java/in/stackroute/hackathon/moviebackend/favourite/controller/FavouriteController.java
+++ src/main/java/in/stackroute/hackathon/moviebackend/favourite/controller/FavouriteController.java
@@ -13,6 +13,7 @@ import java.util.Map;
 import java.util.TreeMap;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("api/v1")
 public class FavouriteController {
 
